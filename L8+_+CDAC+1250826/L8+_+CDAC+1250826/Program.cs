e3808c54685546f659a6a4034e2de71396f0ea85
Carlos Angulo <cdangulo08@gmail.com>
2026-03-12 15:38:33 -0600

    Primer commit

---
+++ L8+_+CDAC+1250826/L8+_+CDAC+1250826/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Problema #1 - Solicitar 20 números. Identificar el mayor, el menor y el promedio de los números
+        int num;
+        int num_menor = 0;
+        int num_mayor = 0;
+        double sumatoria = 0;
+
+        for(int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("Ingrese un número");
+            string? dato = Console.ReadLine();
+            num = int.Parse(dato!);
+
+            sumatoria += num;
+
+            if(i == 0)
+            {
+                num_menor = num;
+                num_mayor = num;
+            }else if(num < num_menor)
+            {
+                num_menor = num;
+            }else if(num > num_mayor)
+            {
+                num_mayor = num;
+            }
+        }
+
+        double promedio = sumatoria / 20;
+
+        Console.WriteLine("El número más grande es " + num_mayor);
+        Console.WriteLine("El número más pequeño es " + num_menor);
+        Console.WriteLine("El promedio de todos los números es de " + promedio);
+    }
+}
