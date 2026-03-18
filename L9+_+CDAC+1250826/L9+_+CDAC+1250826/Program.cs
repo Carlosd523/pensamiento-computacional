4a95afcd545cb9419a875fa1835d096464128b85
Carlos Angulo <cdangulo08@gmail.com>
2026-03-18 16:52:59 -0600

    Agregación - Problema #4
    Se agrega el problema 4 del laboratorio

--- L9+_+CDAC+1250826/L9+_+CDAC+1250826/Program.cs
+++ L9+_+CDAC+1250826/L9+_+CDAC+1250826/Program.cs
@@ -22,6 +22,72 @@ class Program
         return e;
     }
 
+    // Funciones - Problema #4
+    static int recibirDaño(ref int puntosSalud)
+    {
+        if(puntosSalud - 5 >= 0)
+        {
+            puntosSalud = puntosSalud - 5;
+        }
+        else
+        {
+            puntosSalud = puntosSalud - puntosSalud;
+        }
+        return puntosSalud;
+    }
+
+    static int curar(ref int puntosSalud)
+    {
+        if(puntosSalud + 3 <= 15)
+        {
+            puntosSalud = puntosSalud + 3;
+        }
+        else
+        {
+            puntosSalud = puntosSalud + (15 - puntosSalud);
+        }
+        return puntosSalud;
+    }
+
+    static void mostrarSalud(int puntosSalud)
+    {
+        if(puntosSalud <= 5 && puntosSalud >= 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Su salud es de " + puntosSalud + "pts");
+            Console.ResetColor();
+        }else if(puntosSalud > 5 && puntosSalud <= 10)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Su salud es de " + puntosSalud + "pts");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Su salud es de " + puntosSalud + "pts");
+            Console.ResetColor();
+        }
+        return;
+    }
+
+    static string calificarDesempeño(int puntosSalud)
+    {
+        if(puntosSalud >= 0 && puntosSalud <= 5)
+        {
+            return "C";
+        }else if(puntosSalud > 5 && puntosSalud <= 10)
+        {
+            return "B";
+        }else if(puntosSalud > 10 && puntosSalud <= 14)
+        {
+            return "A";
+        }
+        else
+        {
+            return "S";
+        }
+    }
     static void Main()
     {
         // Main - Problema #1
@@ -46,6 +112,10 @@ class Program
             "\n" + variable1 + " para la primera variable" +
             "\n" + variable2 + " para la segunda variable");
         cambioValores(ref variable1, ref variable2);
+
+        Console.WriteLine("Presione Enter para continuar");
+        Console.ReadLine();
+
         Console.WriteLine("El valor después de haber cambiado los valores es de: " +
             "\n" + variable1 + " para la primera variable" +
             "\n" + variable2 + " para la segunda variable");
@@ -89,8 +159,8 @@ class Program
         do
         {
             Console.WriteLine("¿Usted tiene membresía premium? 0 = No / 1 = Sí");
-            string dato5 = Console.ReadLine()!;
-            membresia = int.Parse(dato5);
+            string dato6 = Console.ReadLine()!;
+            membresia = int.Parse(dato6);
         } while (membresia < 0 || membresia > 1);
 
         if(membresia == 1)
@@ -102,6 +172,86 @@ class Program
         Console.WriteLine("El valor actual de su boleto es: Q." + precio);
         Console.WriteLine("El valor final de su boleto es: Q." + descuentoBoleto(descuento, ref precio));
 
+        Console.WriteLine("Presione Enter para continuar");
+        Console.ReadLine();
+        Console.Clear();
+
+        // Main - Problema #4
+
+        int vida = 15;
+        int opcion = 0;
+
+        Console.WriteLine("BIENVENIDO A ANGULO'S GAME");
+        do
+        {
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Decida qué hará durante el día" +
+                "\n" + "1 = Comer algo" +
+                "\n" + "2 = Luchar contra mounstros" +
+                "\n" + "3 = Mostrar estadísticas" +
+                "\n" + "4 = Salir del juego");
+                Console.ResetColor();
+                string dato7 = Console.ReadLine()!;
+                opcion = int.Parse(dato7);
+
+                if (opcion < 1 || opcion > 4)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Por favor, ingrese un número válido");
+                    Console.ResetColor();
+                }
+            } while (opcion < 1 || opcion > 4);
+
+            switch (opcion)
+            {
+                case 1:
+                    if(vida == 15)
+                    {
+                        Console.WriteLine("Salud al máximo");
+                        Console.WriteLine("Presione Enter para continuar");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+                    Console.WriteLine("Ha comido una manzana");
+                    Console.WriteLine("Su salud ha aumentado");
+                    curar(ref vida);
+
+                    Console.WriteLine("Presione Enter para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+                case 2:
+                    if(vida == 0)
+                    {
+                        Console.WriteLine("No puede luchar contra mounstros actualmente");
+                        Console.WriteLine("Presione Enter para continuar");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+                    Console.WriteLine("Ha luchado contra 10 bokoblins");
+                    Console.WriteLine("Su salud ha disminuido");
+                    recibirDaño(ref vida);
+
+                    Console.WriteLine("Presione Enter para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+                case 3:
+                    Console.WriteLine("A continuación se mostrarán sus estadísticas");
+                    mostrarSalud(vida);
+
+                    Console.WriteLine("Presione Enter para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+            }
+        } while (opcion != 4);
+        Console.WriteLine("Muchas gracias por haber jugado" +
+            "\n" + "Su desempeño fue " + calificarDesempeño(vida));
         Console.WriteLine("Presione Enter para continuar");
         Console.ReadLine();
         Console.Clear();
