0ab06eacb964046e2d2a206a8df5e8f3cb473fe6
Carlos Angulo <cdangulo08@gmail.com>
2026-03-18 09:07:56 -0600

    Ejemplo Lab - Agregación Lab 9

---
+++ L9+_+CDAC+1250826/L9+_+CDAC+1250826/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static int cantidadCaracteres(string a)
+    {
+        int caracteres = a.Length;
+        return caracteres;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un string a evaluar");
+        string texto = Console.ReadLine()!;
+        Console.WriteLine("Su string tiene " + cantidadCaracteres(texto) + "carácteres");
+    }
+}
