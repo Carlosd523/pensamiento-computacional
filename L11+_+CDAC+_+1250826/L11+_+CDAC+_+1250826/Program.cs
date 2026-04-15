5e86607cb695946d0c2f07c03bbb07a7e0858b39
Carlos Angulo <cdangulo08@gmail.com>
2026-04-15 11:41:12 -0600

    Ejercicio 1 - Problema 1

---
+++ L11+_+CDAC+_+1250826/L11+_+CDAC+_+1250826/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    // Funciones - Ejercicio #1
+    static string palabraInversa(string a)
+    {
+        string palabra_inversa = "";
+        for (int i = a.Length; i > 0; i--)
+        {
+            palabra_inversa = palabra_inversa + a[i - 1];
+        }
+        return palabra_inversa;
+    }
+    static Boolean palindromo(string a, string b)
+    {
+        if(a.ToLower() == b.ToLower())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static void Main()
+    {
+        //Ejercicio #1
+        Console.WriteLine("Ingrese una palabra");
+        string palabra = Console.ReadLine()!;
+        Console.WriteLine(palindromo(palabra, palabraInversa(palabra)));
+    }
+}
